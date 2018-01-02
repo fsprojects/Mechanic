5c043bd8129790c9039c83cc079e7f1c40fdd2c0
Tomas Lycken <tlycken@users.noreply.github.com>
2018-01-02 17:17:15 +0100

    Build script builds and runs tests

--- src/build.fsx
+++ src/build.fsx
@@ -6,20 +6,49 @@ nuget FSharp.Core prerelease
 -- Fake Dependencies -- *)
 
 #r @"packages/build/FAKE/tools/FakeLib.dll"
+open System
+open System.Diagnostics
 
+open Fake
 open Fake.Core
 open Fake.Core.TargetOperators
+open Fake.Core.Globbing.Operators
 
-let clean _ = printfn "--- Cleaning stuff ---"
-let build _ = printfn "--- Building the app ---"
-let test _ = printfn "--- Running tests ---"
+let dotnetCliVersion = DotNetCli.getVersion()
+let mutable dotnetCliPath = "dotnet"
+let installDotNet _ = dotnetCliPath <- DotNetCli.InstallDotNetSDK dotnetCliVersion
+
+let clean _ =
+  !!"src/**/bin"
+  ++"src/**/obj"
+  |> CleanDirs
+
+let runDotNet args =
+  let proc (info : ProcessStartInfo) =
+    info.FileName <- dotnetCliPath
+    info.WorkingDirectory <- "."
+    info.Arguments <- args
+
+  printfn "Calling dotnet %s" args
+  let result = ExecProcess proc TimeSpan.MaxValue
+
+  if result <> 0 then failwithf "dotnet %s failed" args
+
+let build _ =
+  runDotNet "build"
+let test _ =
+  !!"**/*Tests.fsproj"
+  |> Seq.map (sprintf "test \"%s\" --no-build")
+  |> Seq.iter runDotNet
 
 Target.Create "Clean" clean
+Target.Create "InstallDotNetCore" installDotNet
 Target.Create "Build" build
 Target.Create "Test" test
 
 
 "Clean"
+  ==> "InstallDotNetCore"
   ==> "Build"
   ==> "Test"
 

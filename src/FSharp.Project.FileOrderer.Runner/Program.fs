5d7756bb746fe216ad12f5d0b86358ecee01e730
Tomas Lycken <tlycken@users.noreply.github.com>
2018-01-02 13:26:46 +0100

    Add hello-world with sln and tests

---
+++ src/FSharp.Project.FileOrderer.Runner/Program.fs
@@ -0,0 +1,6 @@
+﻿open FSharp.Project.FileOrderer
+
+[<EntryPoint>]
+let main argv =
+    Say.hello "World from F#" |> printfn "%s"
+    0
---
+++ src/FSharp.Project.FileOrderer/Library.fs
@@ -0,0 +1,5 @@
+namespace FSharp.Project.FileOrderer
+
+module Say =
+    let hello name =
+        sprintf "Hello %s" name

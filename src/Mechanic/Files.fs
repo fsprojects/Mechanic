3b0a8f0f0809cab7267d1da481cf7782b4c10723
Patrick Donovan <donovan.patrick.j@gmail.com>
2018-01-04 18:01:38 +0000

    Added types to represent project and source files and logic to parse Compile Include tags from the project file

---
+++ src/Mechanic/Files.fs
@@ -0,0 +1,63 @@
+namespace Mechanic.Files
+
+open System.IO
+open System.Xml
+
+
+type ProjectFile = {
+    FileName : string
+    Document : XmlDocument
+}
+
+type SourceFile = SourceFile of fileName:string
+
+
+module ProjectFile =
+
+    let [<Literal>] CompileTag    = "Compile"
+    let [<Literal>] IncludeAttribute = "Include"
+    let [<Literal>] XmlSchema = "http://schemas.microsoft.com/developer/msbuild/2003"
+
+    let inline getNodes name (node:XmlNode) =
+        let xpath = sprintf "*[local-name() = '%s']" name
+        match node.SelectNodes(xpath) with
+        | null -> []
+        | nodeList -> 
+            nodeList
+            |> Seq.cast<XmlNode>
+            |> Seq.toList
+
+    let loadFromStream fileName (stream:Stream) =
+        let doc = XmlDocument()
+        doc.Load stream
+        let ns = XmlNamespaceManager(doc.NameTable)
+        ns.AddNamespace("ns", XmlSchema)
+        { FileName = fileName; Document = doc}
+
+    let loadFromFile fileName =
+        let fi = FileInfo fileName
+        use stream = fi.OpenRead()
+        loadFromStream fi.FullName stream
+
+    let tryLoad fileName =
+        try
+            let fi = FileInfo fileName
+            match fi.Extension with
+            | ".fsproj" -> loadFromFile fi.FullName |> Some
+            | _ -> None
+        with
+        | exn -> None
+
+    let parseFileNames (tag:string) (attr:string) (doc:XmlDocument) =
+        getNodes tag doc
+        |> List.map (fun x ->
+            x.Attributes.[attr].Value)
+
+    let getSourceFiles (fileName:string) (doc:XmlDocument) =
+        let dir = Path.GetDirectoryName fileName
+        parseFileNames CompileTag IncludeAttribute doc
+        |> List.map (fun x ->
+            let fi = FileInfo (Path.Combine(dir, x))
+            SourceFile fi.FullName)
+
+    

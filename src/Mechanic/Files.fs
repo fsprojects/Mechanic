3ff67a8219b039a0352a2fec4df9d512ab782283
Patrick Donovan <donovan.patrick.j@gmail.com>
2018-01-04 23:21:49 +0000

    Added logic to update project file and save it

--- src/Mechanic/Files.fs
+++ src/Mechanic/Files.fs
@@ -5,34 +5,38 @@ open System.Xml
 
 
 type ProjectFile = {
-    FileName : string
-    Document : XmlDocument
+    FileName    : string
+    ProjectNode : XmlNode
+    Document    : XmlDocument
 }
 
-type SourceFile = SourceFile of fileName:string
+type SourceFile = {
+    FullName : string
+    ShortName : string 
+}
 
 
 module ProjectFile =
 
-    let [<Literal>] CompileTag    = "Compile"
-    let [<Literal>] IncludeAttribute = "Include"
-    let [<Literal>] XmlSchema = "http://schemas.microsoft.com/developer/msbuild/2003"
+    open Mechanic.Xml
 
-    let inline getNodes name (node:XmlNode) =
-        let xpath = sprintf ".//*[local-name() = '%s']" name
-        match node.SelectNodes(xpath) with
-        | null -> []
-        | nodeList -> 
-            nodeList
-            |> Seq.cast<XmlNode>
-            |> Seq.toList
+    let [<Literal>] ProjectTag       = "Project"
+    let [<Literal>] ItemGroupTag     = "ItemGroup"
+    let [<Literal>] CompileTag       = "Compile"
+    let [<Literal>] IncludeAttribute = "Include"
+    let [<Literal>] XmlSchema        = "http://schemas.microsoft.com/developer/msbuild/2003"
+
 
     let loadFromStream fileName (stream:Stream) =
         let doc = XmlDocument()
         doc.Load stream
         let ns = XmlNamespaceManager(doc.NameTable)
         ns.AddNamespace("ns", XmlSchema)
-        { FileName = fileName; Document = doc}
+        getNode ProjectTag doc
+        |> function
+           | Some n ->
+             { FileName = fileName; ProjectNode = n; Document = doc}
+           | _ -> failwith "Could not locate project node in project File"
 
     let loadFromFile fileName =
         let fi = FileInfo fileName
@@ -48,16 +52,59 @@ module ProjectFile =
         with
         | exn -> None
 
-    let parseFileNames (tag:string) (attr:string) (doc:XmlDocument) =
-        getNodes tag doc
-        |> List.map (fun x ->
-            x.Attributes.[attr].Value)
+    let save (pf:ProjectFile) =
+        use f = File.Open(pf.FileName, FileMode.Create)
+        use sw = new StreamWriter(f)
+        pf.Document.Save sw
+
+    let getCompileGroup (node:XmlNode) =
+        getDescendants ItemGroupTag node
+        |> Seq.tryFind (hasChildNodes CompileTag)
+
+    let parseSourceFileNames (node:XmlNode) =
+        getCompileGroup node
+        |> Option.map (getChildNodes >> (Seq.choose (getAttribute IncludeAttribute)))
+        |> Option.defaultValue Seq.empty<string>
+        |> List.ofSeq
 
     let getSourceFiles (pFile:ProjectFile) =
-        let dir = Path.GetDirectoryName pFile.FileName
-        parseFileNames CompileTag IncludeAttribute pFile.Document
+        parseSourceFileNames pFile.ProjectNode
         |> List.map (fun x ->
-            let fi = FileInfo (Path.Combine(dir, x))
-            SourceFile fi.FullName)
+            let fi = FileInfo x
+            { FullName  = fi.FullName
+              ShortName = x })
+
+    let makeNode tag (doc:XmlDocument) =
+        doc.CreateElement tag
+
+    let makeCompileNode fileName (doc:XmlDocument) =
+        let node =makeNode CompileTag doc
+        addAttribute IncludeAttribute fileName node
+        
+
+    let updateProjectFile (sFiles:SourceFile list) (pf:ProjectFile) =
+        let rec addCompileNodes files (parent:XmlNode) =
+            match files with
+            | [] -> parent
+            | x::xs ->
+                makeCompileNode x.ShortName pf.Document
+                |> parent.AppendChild |> ignore
+                addCompileNodes xs parent
+
+        let createNewItemGroup (sFiles:SourceFile list) (pf:ProjectFile) =
+            let parent = makeNode ItemGroupTag pf.Document
+            addCompileNodes sFiles parent
+            |> pf.ProjectNode.AppendChild
+
+        let cg = getCompileGroup pf.ProjectNode
+        match cg with
+        | Some x ->
+            pf.ProjectNode.RemoveChild x |> ignore
+            createNewItemGroup sFiles pf |> ignore
+        | None -> 
+            createNewItemGroup sFiles pf |> ignore
+
+        save pf
+
 
     

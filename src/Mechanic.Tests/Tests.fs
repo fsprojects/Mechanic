083a2f7a1eeae41da87b4de39fc09ec1cc429ce7
jindraivanek <jindra.ivanek@gmail.com>
2018-01-07 19:58:00 +0100

    Topological order alg property test.

--- src/Mechanic.Tests/Tests.fs
+++ src/Mechanic.Tests/Tests.fs
@@ -3,9 +3,31 @@ module Tests
 open FsCheck.Xunit
 open Swensen.Unquote
 open Mechanic
+open Mechanic.GraphAlg
 
 [<Property>]
 let ``Say says hello to whomever we greet`` greetee =
     let greeting = Say.hello greetee
     let expected = "Hello " + greetee
     greeting =! expected
+
+[<Property>]
+let ``Topological order alg`` (edges: list<int * int>) =
+    let rec haveCycleAcc edges acc =
+        let (edgesFrom, edgesRemain) = edges |> List.partition (fun (v,_) -> Set.contains v acc)
+        match edgesFrom with
+        | [] -> false
+        | _ ->
+        let nodes = edgesFrom |> Seq.map (fun (_,w) -> w) |> set
+        match Set.intersect nodes acc |> Set.isEmpty with
+        | true -> haveCycleAcc edgesRemain (acc + nodes)
+        | false -> true
+    let haveCycle (nodes: list<int>) edges = nodes |> Seq.exists (fun v -> haveCycleAcc edges (set [v]))
+
+    let nodes = edges |> List.collect (fun (v,w) -> [v;w]) |> List.distinct
+    match GraphAlg.topologicalOrder nodes edges with
+    | TopologicalOrder order ->
+        List.length nodes =! List.length order
+        let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
+        <@ edges |> Seq.forall (fun (v,w) -> orderPos.[v] < orderPos.[w]) @> |> test
+    | Cycle _ -> <@ haveCycle nodes edges @> |> test

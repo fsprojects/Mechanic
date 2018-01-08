9ea030c037372b0c68d7af9a1bac9b8387936c13
Patrick Donovan <donovan.patrick.j@gmail.com>
2018-01-08 20:36:02 +0000

    Changed test to only check on win32 platform

--- src/Mechanic.Tests/Environment.fs
+++ src/Mechanic.Tests/Environment.fs
@@ -9,9 +9,11 @@ open Mechanic.Environment
      testList "Project file tests" [
  
          testCase "Paths and filename are combined correctly" <| fun _ ->
-            let path = 
-                join [programFiles; SystemLibLocation]
-                |> pathCombine (toDll "TestLib")
-            let expected = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETCore\v4.5.1\TestLib.dll"
-            Expect.equal path expected "Paths combined correctly"
+            if isWin32 then
+                // Need to sort out the location of the reference assemblies on other platforms
+                let path = 
+                    join [programFiles; SystemLibLocation]
+                    |> pathCombine (toDll "TestLib")
+                let expected = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETCore\v4.5.1\TestLib.dll"
+                Expect.equal path expected "Paths combined correctly"
      ]

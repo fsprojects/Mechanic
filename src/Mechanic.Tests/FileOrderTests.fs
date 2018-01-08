9dee25bb15269b58e51ed65fe62297133d19a04a
jindraivanek <jindra.ivanek@gmail.com>
2018-01-08 10:56:13 +0100

    after rebase - switch tests to Expecto

--- src/Mechanic.Tests/FileOrderTests.fs
+++ src/Mechanic.Tests/FileOrderTests.fs
@@ -1,7 +1,6 @@
 module Tests.FileOrder
 
-open Xunit
-open Swensen.Unquote
+open Expecto
 open Mechanic
 open Mechanic.GraphAlg
 open System.IO
@@ -30,114 +29,117 @@ let makeTempProject source1 source2 =
 
 let checkOrder source1 source2 =
     let (_, _, f2, f1) = makeTempProject source2 source1
-    SymbolGraph.solveOrder [f1; f2] =! TopologicalOrder [f1; f2]
-    SymbolGraph.solveOrder [f2; f1] =! TopologicalOrder [f1; f2]
+    Expect.equal (SymbolGraph.solveOrder [f1; f2]) (TopologicalOrder [f1; f2]) "Wrong order of files"
+    Expect.equal (SymbolGraph.solveOrder [f2; f1]) (TopologicalOrder [f1; f2]) "Wrong order of files"
 
 let checkCycle source1 source2 =
     let (_, _, f1, f2) = makeTempProject source1 source2
-    <@ match SymbolGraph.solveOrder [f2; f1] with
-       | Cycle _ -> true
-       | _ -> false @>
-    |> test
+    match SymbolGraph.solveOrder [f2; f1] with
+    | Cycle _ -> true
+    | _ -> false
+    |> fun x -> Expect.isTrue x "Dependency cycle expected"
 
 
-[<Fact>]
-let ``file order test 1``() =
-    let source1 = """module Test1
-let x = 42
-"""
-    let source2 = """module Test2
-open Test1
-let y = x
-"""
-    checkOrder source1 source2
+[<Tests>]
+ let tests =
+    testList "FileOrder" [
+        test "file order test 1" {
+            let source1 = """module Test1
+        let x = 42
+        """
+            let source2 = """module Test2
+        open Test1
+        let y = x
+        """
+            checkOrder source1 source2
+        }
 
-[<Fact>]
-let ``file order test 2``() =
-    let source1 = """module Test1
-let x = 42
-"""
-    let source2 = """module Test2
-let y = Test1.x
-"""
-    checkOrder source1 source2
+        test "file order test 2" {
+            let source1 = """module Test1
+        let x = 42
+        """
+            let source2 = """module Test2
+        let y = Test1.x
+        """
+            checkOrder source1 source2
+        }
 
-[<Fact>]
-let ``file order test 3``() =
-    let source1 = """module Test1
-module M =
-    let x = 42
-"""
-    let source2 = """module Test2
-open Test1
-let y = M.x
-"""
-    checkOrder source1 source2
+        test "file order test 3" {
+            let source1 = """module Test1
+        module M =
+            let x = 42
+        """
+            let source2 = """module Test2
+        open Test1
+        let y = M.x
+        """
+            checkOrder source1 source2
+        }
 
-[<Fact>]
-let ``file order test 4``() =
-    let source1 = """module Test1
-module M =
-    let x = 42
-"""
-    let source2 = """module Test2
-open Test1.M
-let y = x
-"""
-    checkOrder source1 source2
+        test "file order test 4" {
+            let source1 = """module Test1
+        module M =
+            let x = 42
+        """
+            let source2 = """module Test2
+        open Test1.M
+        let y = x
+        """
+            checkOrder source1 source2
+        }        
 
-[<Fact>]
-let ``file order test 5``() =
-    let source1 = """module Test1
-module M =
-    let x = 42
-"""
-    let source2 = """module Test2
-let y = Test1.M.x
-"""
-    checkOrder source1 source2
+        test "file order test 5" {
+            let source1 = """module Test1
+        module M =
+            let x = 42
+        """
+            let source2 = """module Test2
+        let y = Test1.M.x
+        """
+            checkOrder source1 source2
+        }
 
-[<Fact>]
-let ``file order test 6``() =
-    let source1 = """module Test1
-module M =
-    let x = 42
-"""
-    let source2 = """module Test2
-open Test1.M
-let y = M.x
-"""
-    checkOrder source1 source2
+        test "file order test 6" {
+            let source1 = """module Test1
+        module M =
+            let x = 42
+        """
+            let source2 = """module Test2
+        open Test1.M
+        let y = M.x
+        """
+            checkOrder source1 source2
+        }        
 
-[<Fact>]
-let ``file order test 7``() =
-    let source1 = """module Test1.M
-let x = 42
-"""
-    let source2 = """module Test2
-let y = M.x
-"""
-    checkOrder source1 source2
+        test "file order test 7" {
+            let source1 = """module Test1.M
+        let x = 42
+        """
+            let source2 = """module Test2
+        let y = M.x
+        """
+            checkOrder source1 source2
+        }        
 
-[<Fact>]
-let ``file order test 8``() =
-    let source1 = """namespace Test
-module M =
-    let x = 42
-"""
-    let source2 = """namespace Test
-module M2 =
-    let y = M.x
-"""
-    checkOrder source1 source2
+        test "file order test 8" {
+            let source1 = """namespace Test
+        module M =
+            let x = 42
+        """
+            let source2 = """namespace Test
+        module M2 =
+            let y = M.x
+        """
+            checkOrder source1 source2
+        }        
 
-
-[<Fact>]
-let ``file order test - cycle``() =
-    let source1 = """module Test1
-let x = Test2.y
-"""
-    let source2 = """module Test2
-let y = Test1.x
-"""
-    checkCycle source1 source2
+        test "file order test cycle" {
+            let source1 = """module Test1
+        let x = Test2.y
+        """
+            let source2 = """module Test2
+        let y = Test1.x
+        """
+            checkCycle source1 source2
+        }
+    ]
--- src/Mechanic.Tests/Tests.fs
+++ src/Mechanic.Tests/Tests.fs
@@ -1,28 +1,30 @@
 module Tests.Main
 
-open FsCheck.Xunit
-open Swensen.Unquote
+open Expecto
 open Mechanic
 open Mechanic.GraphAlg
 
-[<Property>]
-let ``Topological order alg`` (edges: list<int * int>) =
-    let rec haveCycleAcc edges acc =
-        let (edgesFrom, edgesRemain) = edges |> List.partition (fun (v,_) -> Set.contains v acc)
-        match edgesFrom with
-        | [] -> false
-        | _ ->
-        let nodes = edgesFrom |> Seq.map (fun (_,w) -> w) |> set
-        match Set.intersect nodes acc |> Set.isEmpty with
-        | true -> haveCycleAcc edgesRemain (acc + nodes)
-        | false -> true
-    let haveCycle (nodes: list<int>) edges = nodes |> Seq.exists (fun v -> haveCycleAcc edges (set [v]))
+[<Tests>]
+ let tests =
+    testList "GraphAlg" [
+        testProperty "Topological order alg" <| fun (edges: list<int * int>) ->
+            let rec haveCycleAcc edges acc =
+                let (edgesFrom, edgesRemain) = edges |> List.partition (fun (v,_) -> Set.contains v acc)
+                match edgesFrom with
+                | [] -> false
+                | _ ->
+                let nodes = edgesFrom |> Seq.map (fun (_,w) -> w) |> set
+                match Set.intersect nodes acc |> Set.isEmpty with
+                | true -> haveCycleAcc edgesRemain (acc + nodes)
+                | false -> true
+            let haveCycle (nodes: list<int>) edges = nodes |> Seq.exists (fun v -> haveCycleAcc edges (set [v]))
 
-    let nodes = edges |> List.collect (fun (v,w) -> [v;w]) |> List.distinct
-    match GraphAlg.topologicalOrder nodes edges with
-    | TopologicalOrder order ->
-        List.length nodes =! List.length order
-        let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
-        <@ edges |> Seq.forall (fun (v,w) -> orderPos.[v] < orderPos.[w]) @> |> test
-    | Cycle _ -> <@ haveCycle nodes edges @> |> test
+            let nodes = edges |> List.collect (fun (v,w) -> [v;w]) |> List.distinct
+            match GraphAlg.topologicalOrder nodes edges with
+            | TopologicalOrder order ->
+                Expect.equal (List.length order) (List.length nodes) "Number of nodes differs"
+                let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
+                Expect.all edges (fun (v,w) -> orderPos.[v] < orderPos.[w]) "Ordering must respect oriented edge"
+            | Cycle _ -> Expect.isTrue (haveCycle nodes edges) "Cycle reported on graph without cycle"
+    ]
 

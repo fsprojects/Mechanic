537c005c47fff436bd5f208775d4e72d0954b6c7
jindraivanek <jindra.ivanek@gmail.com>
2018-01-16 08:07:15 +0100

    Allow to load files fro project

--- src/Mechanic.CommandLine/Program.fs
+++ src/Mechanic.CommandLine/Program.fs
@@ -1,9 +1,19 @@
 ﻿open Mechanic
+open Mechanic.Files
 
 [<EntryPoint>]
 let main argv =
-    let root = argv.[0]
-    let pattern = argv.[1]
-    SymbolGraph.solveOrderFromPattern root pattern 
-    |> printfn "%A"
+    match argv.Length with
+    | 1 ->
+        ProjectFile.loadFromFile argv.[0]
+        |> ProjectFile.getSourceFiles
+        |> List.map (fun f -> f.FullName)
+        |> List.filter (fun x -> x.EndsWith ".fs")
+        |> SymbolGraph.solveOrder
+        |> printfn "%A"
+    | 2 ->
+        let root = argv.[0]
+        let pattern = argv.[1]
+        SymbolGraph.solveOrderFromPattern root pattern 
+        |> printfn "%A"
     0

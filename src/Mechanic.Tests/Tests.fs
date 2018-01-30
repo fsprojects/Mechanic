967fc669716cc8c3d0ebf388a1d4c4889534eada
jindraivanek <jindra.ivanek@gmail.com>
2018-01-30 17:54:11 +0100

    Fix bug in min edit distance test

--- src/Mechanic.Tests/Tests.fs
+++ src/Mechanic.Tests/Tests.fs
@@ -26,10 +26,31 @@ let correctOrder edges order =
     let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
     Seq.forall (fun (v,w) -> orderPos.[v] < orderPos.[w]) edges
 
+let checkMinDist nodes edges =
+    let edges = edges |> List.filter (fun (v,w) -> v <> w)
+    let variants = nodes |> List.allPermutations |> List.filter (correctOrder edges)
+    let editDistance order1 order2 =
+        let rec f xs ys =
+            let orderPos1 = xs |> List.mapi (fun i v -> v, i) |> Map.ofList
+            match ys with
+            | [] -> 0
+            | (y::ys) -> orderPos1.[y] + f (xs |> List.filter ((<>)y)) ys
+        f order1 order2
+    match variants with
+    | [] -> ()
+    | _ ->
+    let minOrder = variants |> List.minBy (editDistance nodes)
+    let minDistance = editDistance nodes minOrder
+    printfn "%A" (minOrder, minDistance)
+    match GraphAlg.topologicalOrder nodes edges with
+    | TopologicalOrder order ->
+        Expect.equal (editDistance nodes order) minDistance (sprintf "Not minimal edit distance: original %A result %A min dist %A" nodes order minOrder)
+    | _ -> ()
+
 [<Tests>]
  let tests =
     testList "GraphAlg" [
-        testPropertyWithConfig (Gen.addToConfig {FsCheckConfig.defaultConfig with maxTest = 100; endSize = 100}) "Topological order alg - correctness" <| fun (Gen.RandomGraph(nodes, edges)) ->
+        testPropertyWithConfig (Gen.addToConfig {FsCheckConfig.defaultConfig with maxTest = 100; endSize = 50}) "Topological order alg - correctness" <| fun (Gen.RandomGraph(nodes, edges)) ->
             let rec haveCycleAcc edges acc =
                 let (edgesFrom, edgesRemain) = edges |> List.partition (fun (v,_) -> Set.contains v acc)
                 match edgesFrom with
@@ -45,27 +66,15 @@ let correctOrder edges order =
             | TopologicalOrder order ->
                 Expect.equal (List.length order) (List.length nodes) "Number of nodes differs"
                 let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
-                Expect.all edges (fun (v,w) -> orderPos.[v] < orderPos.[w]) "Ordering must respect oriented edge"
+                Expect.all edges (fun (v,w) -> orderPos.[v] <= orderPos.[w]) "Ordering must respect oriented edge"
             | Cycle _ -> Expect.isTrue (haveCycle nodes edges) "Cycle reported on graph without cycle"
-
+        
         // this test is really slow for bigger sizes, because it check all permutations of given size
-        testPropertyWithConfig (Gen.addToConfig {FsCheckConfig.defaultConfig with maxTest = 100; endSize = 7}) "Topological order alg - min edit distance" <| fun (Gen.RandomGraph(nodes, edges)) ->
-            let edges = edges |> List.filter (fun (v,w) -> v <> w)
-            let variants = nodes |> List.allPermutations |> List.filter (correctOrder edges)
-            let editDistance order1 order2 =
-                let orderPos1 = order1 |> List.mapi (fun i v -> v, i) |> Map.ofList
-                let rec f i = function
-                    | [] -> 0
-                    | (x::xs) -> orderPos1.[x] - i + f (i+1) xs
-                f 0 order2
-            match variants with
-            | [] -> ()
-            | _ ->
-            let minOrder = variants |> List.minBy (editDistance nodes)
-            let minDistance = editDistance nodes minOrder
-            match GraphAlg.topologicalOrder nodes edges with
-            | TopologicalOrder order ->
-                Expect.equal (editDistance nodes order) minDistance (sprintf "Not minimal edit distance: original %A result %A min dist %A" nodes order minOrder)
-            | _ -> ()
+        testPropertyWithConfig (Gen.addToConfig {FsCheckConfig.defaultConfig with maxTest = 1000; endSize = 7}) "Topological order alg - min edit distance" <| fun (Gen.RandomGraph(nodes, edges)) ->
+            checkMinDist nodes edges
+
+        test "Topological order alg - min edit distance - unit test 1" {
+            checkMinDist [1..7] [7,1;1,2;2,3]
+        }
     ]
 

fa58217ebdebaef431124e14176ef9b91a7a42b4
jindraivanek <jindra.ivanek@gmail.com>
2018-01-31 17:52:16 +0100

    Revert fix-order experiment

--- src/Mechanic/GraphAlg.fs
+++ src/Mechanic/GraphAlg.fs
@@ -22,36 +22,6 @@ let getMinCycle (nodes: list<_>) edges =
     let edgesMap = edges |> Seq.groupBy fst |> Seq.map (fun (v,g) -> v, g |> Seq.map snd |> Seq.toList) |> Map.ofSeq
     nodes |> List.map (fun v -> getCycleAcc edgesMap [v]) |> choosePick (Seq.minBy (List.length))
 
-let editDistance order1 order2 =
-    let rec f xs ys =
-        let orderPos1 = xs |> List.mapi (fun i v -> v, i) |> Map.ofList
-        match ys with
-        | [] -> 0
-        | (y::ys) -> orderPos1.[y] + f (xs |> List.filter ((<>)y)) ys
-    f order1 order2
-
-let rec fixOrder origOrder edges order =
-    let d = editDistance origOrder order
-    printfn "fixOrder %A" (order, d)
-    let rec move xs (i,j) =
-        if j<i then move (List.rev xs) (j,i) |> List.rev
-        elif i=j then xs
-        else
-        let a = Array.ofList xs
-        let n = a.Length - 1
-        (Array.toList a.[0..i-1]) @ (Array.toList a.[i+1..j]) @ [a.[i]] @ (Array.toList a.[j+1..n])
-
-    [0..(List.length order)-1] |> List.collect (fun i -> [0..(List.length order)-1] |> List.map (fun j -> i,j)) 
-    |> List.map (move order)
-    |> List.filter (fun order -> 
-        let orderPos = order |> List.mapi (fun i v -> v, i) |> Map.ofList
-        edges |> List.exists (fun (v,w) -> orderPos.[w] < orderPos.[v]) |> not)
-    |> List.map (fun x -> d - editDistance origOrder x, x) |> List.filter (fun (d2,_) -> d2 > 0) 
-    |> function
-       | [] -> order
-       | xs -> 
-            xs |> List.maxBy fst |> snd |> fixOrder origOrder edges
-
 let topologicalOrder orderedNodes edges =
     let edges = edges |> List.filter (fun (v,w) -> v<>w)
     let orderPos = orderedNodes |> List.mapi (fun i v -> v, i) |> Map.ofList
@@ -88,7 +58,4 @@ let topologicalOrder orderedNodes edges =
                 ) |> Seq.tryHead
             next |> Option.map solve |> Option.defaultValue (TopologicalOrder acc)
     
-    match solve (nodeInLevel, edges, []) with
-    | TopologicalOrder result ->
-        TopologicalOrder (fixOrder orderedNodes edges result)
-    | x -> x
+    solve (nodeInLevel, edges, [])
